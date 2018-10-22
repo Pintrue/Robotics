0366a703a8352fcb9ebe20a546512eb88ee34709
Pintrue <y451059931@icloud.com>
2018-10-22 14:06:15 +0000

    backup init

---
+++ motor_position_example.py
@@ -0,0 +1,52 @@
+import brickpi
+import time
+
+interface=brickpi.Interface()
+interface.initialize()
+
+motors = [0,1]
+
+interface.motorEnable(motors[0])
+interface.motorEnable(motors[1])
+
+motorParams0 = interface.MotorAngleControllerParameters()
+motorParams0.maxRotationAcceleration = 6.0
+motorParams0.maxRotationSpeed = 12.0
+motorParams0.feedForwardGain = 255/20.0
+motorParams0.minPWM = 18.0
+motorParams0.pidParameters.minOutput = -255
+motorParams0.pidParameters.maxOutput = 255
+motorParams0.pidParameters.k_p = 100.0
+motorParams0.pidParameters.k_i = 0.0
+motorParams0.pidParameters.k_d = 0.0
+
+motorParams1 = interface.MotorAngleControllerParameters()
+motorParams1.maxRotationAcceleration = 6.0
+motorParams1.maxRotationSpeed = 12.0
+motorParams1.feedForwardGain = 255/20.0
+motorParams1.minPWM = 18.0
+motorParams1.pidParameters.minOutput = -255
+motorParams1.pidParameters.maxOutput = 255
+motorParams1.pidParameters.k_p = 100.0
+motorParams1.pidParameters.k_i = 0.0
+motorParams1.pidParameters.k_d = 0.0
+
+
+interface.setMotorAngleControllerParameters(motors[0],motorParams0)
+interface.setMotorAngleControllerParameters(motors[1],motorParams1)
+
+while True:
+	angle = float(input("Enter a angle to rotate (in radians): "))
+
+	interface.increaseMotorAngleReferences(motors,[angle,angle])
+
+	while not interface.motorAngleReferencesReached(motors) :
+		motorAngles = interface.getMotorAngles(motors)
+		if motorAngles :
+			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+		time.sleep(0.1)
+
+	print "Destination reached!"
+	
+
+interface.terminate()

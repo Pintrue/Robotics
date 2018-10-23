7c3a4b43d8886893d1d4df40d0ff2b923f055d59
Pinchu <py416@ic.ac.uk>
2018-10-23 12:50:36 +0000

    adjusted kp value

--- move_square.py
+++ move_square.py
@@ -1,10 +1,12 @@
 import brickpi
 import time
+import math
 
 interface=brickpi.Interface()
 interface.initialize()
 
 motors = [0,1]
+speed = 6.0
 
 interface.motorEnable(motors[0])
 interface.motorEnable(motors[1])
@@ -16,7 +18,7 @@ motorParams0.feedForwardGain = 255/20.0
 motorParams0.minPWM = 18.0
 motorParams0.pidParameters.minOutput = -255
 motorParams0.pidParameters.maxOutput = 255
-motorParams0.pidParameters.k_p = 800.0
+motorParams0.pidParameters.k_p = 100.0
 motorParams0.pidParameters.k_i = 0.0
 motorParams0.pidParameters.k_d = 0.0
 
@@ -27,32 +29,32 @@ motorParams1.feedForwardGain = 255/20.0
 motorParams1.minPWM = 18.0
 motorParams1.pidParameters.minOutput = -255
 motorParams1.pidParameters.maxOutput = 255
-motorParams1.pidParameters.k_p = 800.0
+motorParams1.pidParameters.k_p = 100.0
 motorParams1.pidParameters.k_i = 0.0
 motorParams1.pidParameters.k_d = 0.0
-
 
 interface.setMotorAngleControllerParameters(motors[0],motorParams0)
 interface.setMotorAngleControllerParameters(motors[1],motorParams1)
 
-# start logging time and reference angles of the two motors
-interface.startLogging("./refangle.txt")
+def left90deg():
+    increaseMotorAngleReferences(motors,[math.pi * 2, math.pi * -2])
 
+def right90deg():
+    increaseMotorAngleReferences(motors,[math.pi * -2, math.pi * 2])
+
+def move_in_square():
+    for i in range(4):
+        interface.setMotorRotationSpeedReferences(motors,[speed,speed])
+        time.sleep(4)
+        interface.setMotorRotationSpeedReferences(motors,[0,0])
+        left90deg();
+
+
+print "Press Ctrl+C to exit"
+counter = 0
 while True:
-	angle = float(input("Enter a angle to rotate (in radians): "))
-
-	interface.increaseMotorAngleReferences(motors,[angle,angle])
-
-	while not interface.motorAngleReferencesReached(motors) :
-		motorAngles = interface.getMotorAngles(motors)
-		if motorAngles :
-			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
-		time.sleep(0.1)
-
-	print "Destination reached!"
-	
-# stop logging
-interface.stopLogging("./refangle.txt")
-
-
-interface.terminate()
+        if (input("Enter y to move") == 'y'):
+            move_in_square()
+	    counter += 1
+        else:
+            interface.terminate()

9670beb4b48f194151c2541ef25d4de779495033
Pintrue <y451059931@icloud.com>
2018-11-01 11:24:44 +0000

    test bumper

--- sensor.py
+++ sensor.py
@@ -1,6 +1,7 @@
 import brickpi
 import time
 import math
+import random
 
 interface=brickpi.Interface()
 interface.initialize()
@@ -36,12 +37,19 @@ motorParams1.pidParameters.k_d = 70.0
 interface.setMotorAngleControllerParameters(motors[0],motorParams0)
 interface.setMotorAngleControllerParameters(motors[1],motorParams1)
 
+touch_port_left = 0
+touch_port_right = 1
+interface.sensorEnable(touch_port_left, brickpi.SensorType.SENSOR_TOUCH)
+interface.sensorEnable(touch_port_right, brickpi.SensorType.SENSOR_TOUCH)
+
+
+
 def right90deg():
     interface.increaseMotorAngleReferences(motors,[math.pi * 1.155, math.pi * -1.155])
     while not interface.motorAngleReferencesReached(motors) :
       #motorAngles = interface.getMotorAngles(motors)
       time.sleep(0.1)
-	
+
 def left90deg():
     interface.increaseMotorAngleReferences(motors,[math.pi * -1.155, math.pi * 1.155])
     while not interface.motorAngleReferencesReached(motors) :
@@ -61,53 +69,41 @@ def move_in_square():
         print("turning")
         right90deg()
 
-def turn_test():
-    left90deg()
-    time.sleep(1)
-    move(3)
-    right90deg()
+def stop():
+    interface.setMotorPwm(motors[0], 0)
+    interface.setMotorPwm(motors[1], 0)
 
-def move_test():
-    move(11.6)
-    time.sleep(1)
-    move(-11.6)
+distance = 5
 
-def covariance_matrix(xs, ys):
-    n = len(xs)
-    xs = map(lambda x: float(x), xs)
-    ys = map(lambda x: float(x), ys)
-    mean_x = reduce(lambda x, y: x + y, xs) / n
-    mean_y = reduce(lambda x, y: x + y, ys) / n
-    x_term = round(reduce(lambda x, y: x + y, map(lambda x: (x - mean_x)**2, xs))/ n, 3)
-    y_term = round(reduce(lambda x, y: x + y, map(lambda x: (x - mean_y)**2, ys))/ n, 3)
-    zs = []
-    for i in range(n):
-        zs.append((xs[i] - mean_x) * (ys[i] - mean_y))
-    same_term = round(reduce(lambda x, y: x + y, zs) / n, 3)
-    return [[x_term, same_term],[same_term, y_term]]
-
-print "Press Ctrl+C to exit"
-counter = 0
-exclaimation = ""
 while True:
-    select = input("Enter 1-4 for : square_move, turn_test, move_test, covariance_matrix : ")
-    if select == 1:
-        counter += 1
-        exclaimation += "!"
-        move_in_square()
-        print("You have tried square moving" + str(counter) + " times" + exclaimation)
-    elif select == 2:
-        turn_test()
-        print("Turn test finished!")
-    elif select == 3:
-        move_test()
-        print("Move test finished!")
-    elif select == 4:
-	xs = [1.9, 0.3, -0.7, -0.8, -0.5, -3.3, 0.9, 0.4, -4.0, 0.3]
-	ys = [-4.1, -4.6, -0.3, -0.6, -2.9, 2.5, -2.0, -1.2, -0.3, 0.9]
-	print("Covariance matrix is below: ")
-	print(covariance_matrix(xs, ys))
-    else:
-        break
+    bump1 = interface.getSensorValue(touch_port_left)
+    bump2 = interface.getSensorValue(touch_port_right)
+    if bump1 and bump2:
+        touch1 = bump1[0]
+        touch2 = bump2[0]
+        if touch1 and touch2:
+            stop()
+	    print(touch1, touch2)
+            move(-distance)
+            rand = random.randint(0, 1)
+            if rand:
+                right90deg()
+            else:
+                left90deg()
+        elif touch1 and not touch2:
+            stop()
+	    print(touch1, touch2)
+            move(-distance)
+            right90deg()
+        elif not touch1 and touch2:
+            stop()
+	    print(touch1, touch2)
+            move(-distance)
+            left90deg()
+        else:
+	    pass
+            move(distance)
+            #interface.setMotorRotationSpeedReference(motors, [speed, speed])
+
 interface.terminate()
 
